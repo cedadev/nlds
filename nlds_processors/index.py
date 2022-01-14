9e760e2a13c20347e0f110ec01fa7d1cfdb431e9
Jack Leland <jack.e.leland@gmail.com>
2022-01-14 17:48:07 +0000

    Implement file indexing in index consumer
    A minimum version of file indexing, based on os.walk, is implemented in
    the indexer consumer. Addresses issue #7.

--- nlds_processors/index.py
+++ nlds_processors/index.py
@@ -1,5 +1,8 @@
 import json
+import os
+import pathlib as pth
 from logging import ERROR
+from typing import List
 
 from nlds.rabbit.consumer import RabbitMQConsumer
 from nlds.rabbit.publisher import RabbitMQPublisher
@@ -85,7 +88,66 @@ class IndexerConsumer(RabbitMQConsumer):
             body_json[self.MSG_DATA][self.MSG_ERROR] = str(e)
             new_routing_key = ".".join([self.RK_ROOT, self.RK_MONITOR, self.RK_ERROR])
             self.publish_message(new_routing_key, json.dumps(body_json))
-       
+
+    def index(self, filelist, max_depth=-1):
+        indexed_filelist = []
+
+        # 
+        for item in filelist:
+            item_p = pth.Path(item)
+
+            # index directories by walking them
+            if item_p.is_dir():
+                for directory, subdirs, subfiles in os.walk(item_p):
+                    # Check how deep this iteration has come from starting dir
+                    # and skip if greater than allowed maximum depth
+                    depth = len(pth.Path(directory).relative_to(item_p).parts)
+                    if max_depth >= 0 and depth >= max_depth:
+                        continue
+
+                    for f in subfiles:
+                        indexed_filelist.append(os.path.join(directory, f))
+                        self.check_filelist(indexed_filelist)
+            else:
+                indexed_filelist.append(item_p)
+        
+        # Get the full path
+        abs_root = os.path.abspath(args.dir)
+
+        # Submit items to rabbit queue for re-scan
+        rabbit_connection = RabbitMQConnection(args.conf)
+
+        # Add the root directory
+        msg = rabbit_connection.create_message(abs_root, MKDIR)
+        rabbit_connection.publish_message(msg)
+
+        # If -r flag, walk the whole tree, if not walk only the immediate directory
+        if args.recursive:
+            max_depth = None
+        else:
+            max_depth = 1
+
+        for root, dirs, files in walk_storage_links(abs_root, max_depth=max_depth):
+
+            # Add directories
+            if not args.nodirs:
+                for _dir in dirs:
+                    msg = rabbit_connection.create_message(os.path.join(root, _dir), MKDIR)
+                    rabbit_connection.publish_message(msg)
+
+            # Add files
+            if not args.nofiles:
+                for file in files:
+                    msg = rabbit_connection.create_message(os.path.join(root, file), DEPOSIT)
+                    rabbit_connection.publish_message(msg)
+
+                    if os.path.basename(file) == README:
+                        msg = rabbit_connection.create_message(os.path.join(root, file), README)
+                        rabbit_connection.publish_message(msg)
+
+    def check_filelist(self, filelist: List(str)):
+        if len(filelist) >= self.message_threshold:
+            yield filelist
         
 def main():
     consumer = IndexerConsumer()
