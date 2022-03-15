14a83e4a78b95208753e3d4b70fe7567c240ef5f
Jack Leland <jack.e.leland@gmail.com>
2022-03-15 08:57:14 +0000

    Adds Per-consumer Logging to Separate Files
    The logging of messages to separate files is added, with the log level
    specified as the final part of the routing key (e.g. nlds.log.info) and
    the log target/message specified as additional entries within the DATA
    section of the message body.
    
    Also adds unit tests and fixes the fixtures in conftest used for
    mocking the body and method parameters in consumer callbacks.

--- nlds_processors/logger.py
+++ nlds_processors/logger.py
@@ -3,6 +3,7 @@ import logging
 import sys
 
 from nlds.rabbit.consumer import RabbitMQConsumer
+from nlds.server_config import LOGGING_CONFIG_FILES, LOGGING_CONFIG_LEVEL, LOGGING_CONFIG_SECTION, LOGGING_CONFIG_STDOUT_LEVEL
 
 logger = logging.getLogger(__name__)
 
@@ -12,41 +13,112 @@ class LoggingConsumer(RabbitMQConsumer):
                           f"{RabbitMQConsumer.RK_WILD}"
     DEFAULT_LOGGING_LEVEL = RabbitMQConsumer.RK_LOG_INFO
 
-    _logging_function = {
-        RabbitMQConsumer.RK_LOG_DEBUG: logger.debug,
-        RabbitMQConsumer.RK_LOG_INFO: logger.info,
-        RabbitMQConsumer.RK_LOG_WARNING: logger.warning,
-        RabbitMQConsumer.RK_LOG_ERROR: logger.error,
-        RabbitMQConsumer.RK_LOG_CRITICAL: logger.critical
-    }
-    _logging_levels = {
+    _logging_levels = (
+        RabbitMQConsumer.RK_LOG_DEBUG,
+        RabbitMQConsumer.RK_LOG_INFO,
+        RabbitMQConsumer.RK_LOG_WARNING,
+        RabbitMQConsumer.RK_LOG_ERROR,
+        RabbitMQConsumer.RK_LOG_CRITICAL
+    )
+    _logging_modes = {
         RabbitMQConsumer.RK_LOG_NONE: 0,      
         RabbitMQConsumer.RK_LOG_INFO: 1,
         RabbitMQConsumer.RK_LOG_DEBUG: 2,
+    }
+    _consumers = {
+        
     }
 
     def __init__(self, queue=DEFAULT_QUEUE_NAME, setup_logging_fl=True):
         super().__init__(queue=queue, setup_logging_fl=setup_logging_fl)
 
-        if "logging_level" in self.consumer_config:
-            self.logging_level = self.consumer_config["logging_level"]
+        # The stdout logging confguration controls how much of the output is 
+        # parotted to the terminal. If not set the default is used. 
+        if (LOGGING_CONFIG_SECTION in self.consumer_config 
+           and LOGGING_CONFIG_STDOUT_LEVEL in self.consumer_config[LOGGING_CONFIG_SECTION]):
+            self.logging_config = self.consumer_config[LOGGING_CONFIG_SECTION]
+            self.log_level = self.logging_config[LOGGING_CONFIG_STDOUT_LEVEL]
         else: 
-            self.logging_level = self.DEFAULT_LOGGING_LEVEL
-        self.log_no = self._logging_levels[self.logging_level]
+            self.log_level = self.DEFAULT_LOGGING_LEVEL
+        self.log_mode = self._logging_modes[self.log_level]
         
     def callback(self, ch, method, properties, body, connection):
         # Convert body from bytes to json for ease of manipulation
         body_json = json.loads(body)
 
-        if self.log_no >= 1:
+        # Verify routing key is appropriate
+        try:
+            rk_parts = self.split_routing_key(method.routing_key)
+        except ValueError:
+            logger.error("Routing key inappropriate format, exiting callback.")
+            return
+
+        # Print certain outputs to global logger output depending on the stdout 
+        # log level.
+        if self.log_mode >= 1:
             # Print route information
-            print(f" [x] Received message with route {body_json[self.MSG_DETAILS][self.MSG_ROUTE]}")
-        if self.log_no >= 2:
+            logger.info(f"Received message with route {body_json[self.MSG_DETAILS][self.MSG_ROUTE]}")
+        if self.log_mode >= 2:
             # Print whole json message
-            print(json.dumps(body_json, indent=4))
+            logger.debug(json.dumps(body_json, indent=4))
 
-        print(f" [x] DONE! \n")
+        # The log level should be in the routing key, the logger to use should 
+        # be in the message body under MSG_DATA:MSG_LOG_TARGET
+        if rk_parts[2] not in self._logging_levels:
+            logger.error(f"Invalid routing key provided, log_level is set to an invalid value ({rk_parts[2]})\n"
+                         f"Should be one of {self._logging_levels}.")
+            return
+        
+        try:
+            consumer = body_json[RabbitMQConsumer.MSG_DATA][RabbitMQConsumer.MSG_LOG_TARGET]
+        except KeyError as e:
+            logger.error(f"Invalid message contents, log target should be in the data section of the message body.")
+            return
+        
+        # Get full list of loggers to verify the log_target can actually be used.
+        # Note that this opens up to other defualt loggers able to be targeted.
+        loggers = {name: logging.getLogger(name) for name in logging.root.manager.loggerDict if "nlds" in name}
+        try:
+            consumer_logger = loggers[consumer]
+        except KeyError as e:
+            logger.error(f"Invalid log target provided, consumer does not have a valid logger/handler setup ({rk_parts[1]})\n"
+                         f"Should be one of {loggers.keys()}.")
+            return
+        logging_func = self.get_logging_func(rk_parts[2], logger_like=consumer_logger)
+        
+        # Check message body contains log message, under MSG_DATA:MSG_LOG_MESSAGE
+        try:
+            log_message = body_json[RabbitMQConsumer.MSG_DATA][RabbitMQConsumer.MSG_LOG_MESSAGE]
+        except KeyError as e:
+            logger.error(f"Invalid message contents, log message should be in the data section of the message body.")
+            return
 
+        # Finally, log the message
+        logging_func(log_message)
+
+        logger.info(f"Callback finished. \n")
+
+    @staticmethod
+    def get_logging_func(log_level: str, logger_like: logging.Logger = logger):
+        """
+        Selects the appropriate logging function, given a logging level, from a 
+        Logger object or Logger-like object. The global logger is used by 
+        default (logging.getLogger(__name__)) but anything which has the 
+        standard logging operations (debug, info, warning etc. ) can be used 
+        i.e. the logging package
+
+        :param str log_level:       Logging level requested
+        :param Logger logger_like:  Logger object to get function from
+        :return:    The logging function to use
+        """
+        logging_function = {
+            RabbitMQConsumer.RK_LOG_DEBUG: logger_like.debug,
+            RabbitMQConsumer.RK_LOG_INFO: logger_like.info,
+            RabbitMQConsumer.RK_LOG_WARNING: logger_like.warning,
+            RabbitMQConsumer.RK_LOG_ERROR: logger_like.error,
+            RabbitMQConsumer.RK_LOG_CRITICAL: logger_like.critical
+        }
+        return logging_function[log_level]
         
 def main():
     consumer = LoggingConsumer()
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import os
 import json
 from uuid import UUID
+import logging
 
 import pytest
 
@@ -33,9 +34,24 @@ def default_rmq_method(routing_key="nlds.test.test"):
     return MockRabbitMethod(routing_key=routing_key)
 
 @pytest.fixture
-def default_rmq_message(test_uuid):
+def default_rmq_body(test_uuid):
     return RabbitMQPublisher.create_message(test_uuid, "data", "user", "group", "target")
 
 @pytest.fixture
-def default_rmq_body(default_rmq_message):
-    return json.dumps(default_rmq_message)
+def default_rmq_message_dict(default_rmq_body):
+    return json.loads(default_rmq_body)
+
+@pytest.fixture
+def routed_rmq_message_dict(default_rmq_message_dict):
+    default_rmq_message_dict[RabbitMQPublisher.MSG_DETAILS][RabbitMQPublisher.MSG_ROUTE] = "place->place"
+    return default_rmq_message_dict
+
+@pytest.fixture(autouse=False)
+def no_logs_gte_error(caplog):
+    """
+    Automatically tests/asserts messages where an error or critical level 
+    message is logged.
+    """
+    yield
+    errors = [record for record in caplog.get_records('call') if record.levelno >= logging.ERROR]
+    assert not errors
