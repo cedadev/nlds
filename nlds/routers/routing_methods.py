23027cf0db26074d80b6368573f1c4cf7af1ec65
Jack Leland <jack.e.leland@gmail.com>
2022-03-24 15:25:08 +0000

    Updates to allow additional object store arguments

--- nlds/routers/routing_methods.py
+++ nlds/routers/routing_methods.py
@@ -14,7 +14,8 @@ from uuid import UUID
 rabbit_publisher = RabbitMQPublisher()
 
 def rabbit_publish_response(routing_key: str, transaction_id: UUID, user: str, 
-                            group: str, data: str, target: str = None):
+                            group: str, data: str, tenancy: str, access_key: str, 
+                            secret_key: str, target: str = None):
     rabbit_publisher.get_connection()
     msg = rabbit_publisher.create_message(transaction_id, data, user=user, 
                                           group=group, target=target)
