1ef5c8dd288da87c3e04736baeceb37dd31b5276
Jack Leland <jack.e.leland@gmail.com>
2022-07-20 11:47:37 +0100

    Add put command into transfer processor

--- nlds_processors/transfer.py
+++ nlds_processors/transfer.py
@@ -1,6 +1,7 @@
 import json
 import traceback
 import os
+import pathlib as pth
 from typing import List, NamedTuple
 
 import minio
@@ -14,24 +15,32 @@ class BaseTransferConsumer(RabbitMQConsumer):
     DEFAULT_ROUTING_KEY = (f"{RabbitMQConsumer.RK_ROOT}."
                            f"{RabbitMQConsumer.RK_TRANSFER}."
                            f"{RabbitMQConsumer.RK_WILD}")
+    DEFAULT_REROUTING_INFO = f"->{DEFAULT_QUEUE_NAME.upper()}"
 
     _TENANCY = "tenancy"
     _REQUIRE_SECURE = "require_secure_fl"
     _CHECK_PERMISSIONS = "check_permissions_fl"
+    _PRINT_TRACEBACKS = "print_tracebacks_fl"
+    _REMOVE_ROOT_SLASH = "remove_root_slash_fl"
     DEFAULT_CONSUMER_CONFIG = {
         _TENANCY: None,
         _REQUIRE_SECURE: True,
         _CHECK_PERMISSIONS: True,
+        _PRINT_TRACEBACKS: False,
+        _REMOVE_ROOT_SLASH: True,
     }
 
     def __init__(self, queue=DEFAULT_QUEUE_NAME):
         super().__init__(queue=queue)
 
-        self.tenancy = self.load_config_value(self._TENANCY_URL)
+        self.tenancy = self.load_config_value(self._TENANCY)
         self.require_secure_fl = self.load_config_value(self._REQUIRE_SECURE)
         self.check_permissions_fl = self.load_config_value(
-            self._CHECK_PERMISSIONS
-        )
+            self._CHECK_PERMISSIONS)
+        self.print_tracebacks_fl = self.load_config_value(
+            self._PRINT_TRACEBACKS)
+        self.remove_root_slash_fl = self.load_config_value(
+            self._REMOVE_ROOT_SLASH)
         
     def callback(self, ch, method, properties, body, connection):
         try:
@@ -39,9 +48,9 @@ class BaseTransferConsumer(RabbitMQConsumer):
             body = body.decode("utf-8")
             body_json = json.loads(body)
 
-            self.log(f"Received {body} from {self.queues[0].name} "
-                    f"({method.routing_key})", 
-                    self.RK_LOG_DEBUG)
+            self.log(f"Received {json.dumps(body_json, indent=4)} from "
+                     f"{self.queues[0].name} ({method.routing_key})", 
+                     self.RK_LOG_DEBUG)
 
             # Verify routing key is appropriate
             try:
@@ -77,11 +86,18 @@ class BaseTransferConsumer(RabbitMQConsumer):
                 )
                 return
 
+            tenancy = None
             # If tenancy specified in message details then override the server-
             # config value
-            if self.MSG_TENANCY in body_json[self.MSG_DETAILS]:
-                self.tenancy = body_json[self.MSG_DETAILS][self.MSG_TENANCY]
-            elif self.tenancy is None:
+            if (self.MSG_TENANCY in body_json[self.MSG_DETAILS] 
+                and body_json[self.MSG_DETAILS][self.MSG_TENANCY] is not None):
+                tenancy = body_json[self.MSG_DETAILS][self.MSG_TENANCY]
+            else:
+                tenancy = self.tenancy
+            
+            # Check to see whether tenancy has been specified in either the 
+            # message or the server_config - exit if not. 
+            if tenancy is None:
                 self.log(
                     "No tenancy specified at server- or request-level, exiting "
                     "callback.", 
@@ -89,14 +105,16 @@ class BaseTransferConsumer(RabbitMQConsumer):
                 )
                 return 
 
-            self.log("Starting transfer to object store", self.RK_LOG_INFO)
+            self.log(f"Starting transfer to object store at {tenancy}", 
+                     self.RK_LOG_INFO)
 
             # Append route info to message and then start the transfer
             body_json = self.append_route_info(body_json)
-            self.transfer(transaction_id, access_key, secret_key, filelist)
+            self.transfer(transaction_id, tenancy, access_key, secret_key, 
+                          filelist)
 
-            self.log("Transfer complete, passing list back for cataloguing.", 
-                     self.RK_LOG_INFO)
+            self.log("Transfer complete, passing list back to worker for "
+                     f"re-routing and cataloguing.", self.RK_LOG_INFO)
             
             new_routing_key = ".".join([
                 rk_parts[0], 
@@ -106,7 +124,7 @@ class BaseTransferConsumer(RabbitMQConsumer):
             self.publish_message(new_routing_key, json.dumps(body_json))
         
         except (ValueError, TypeError, KeyError, PermissionError) as e:
-            if self.print_tracebacks:
+            if self.print_tracebacks_fl:
                 tb = traceback.format_exc()
                 self.log(tb, self.RK_LOG_DEBUG)
             self.log(
@@ -114,16 +132,18 @@ class BaseTransferConsumer(RabbitMQConsumer):
                 self.RK_LOG_ERROR, exc_info=e
             )
             body_json[self.MSG_DATA][self.MSG_ERROR] = str(e)
+            body_json[self.MSG_DETAILS][self.MSG_LOG_TARGET] = \
+                f"{self.LOGGER_PREFIX}{self.queue}"
             new_routing_key = ".".join(
                 [self.RK_ROOT, self.RK_LOG, self.RK_LOG_INFO]
             )
             self.publish_message(new_routing_key, json.dumps(body_json))
 
     @retry(S3Error, tries=5, delay=1, logger=None)
-    def transfer(self, transaction_id: str, access_key: str, secret_key: str, 
-                 filelist: List[NamedTuple]):
+    def transfer(self, transaction_id: str, tenancy: str, access_key: str, 
+                 secret_key: str, filelist: List[NamedTuple]):
         client = minio.Minio(
-            self.tenancy,
+            tenancy,
             access_key=access_key,
             secret_key=secret_key,
             secure=self.require_secure_fl,
@@ -147,11 +167,25 @@ class BaseTransferConsumer(RabbitMQConsumer):
                                                            os.R_OK):
                 self.log("File is inaccessible :(", self.RK_LOG_WARNING)
                 # Do failed_list, retry_list stuff
+                return
+
+            self.log(f"Attempting to upload file {indexitem.item}", 
+                     self.RK_LOG_DEBUG)
             
-            client.fput_object(
-                bucket_name, indexitem.item, indexitem.item,
+            # The minio client doesn't like file paths starting at root (i.e. 
+            # with a slash at the very beginning) so it needs to be stripped if 
+            # using minio, and added back on get.
+            # TODO: Add this flag to the file details json metadata 
+            if self.remove_root_slash_fl and indexitem.item[0] == "/":
+                object_name = indexitem.item[1:]
+            else:
+                object_name = indexitem.item
+            
+            result = client.fput_object(
+                bucket_name, object_name, indexitem.item,
             )
-            
+            self.log(f"Successfully uploaded {indexitem.item}", 
+                     self.RK_LOG_DEBUG)
 
 def main():
     consumer = BaseTransferConsumer()
