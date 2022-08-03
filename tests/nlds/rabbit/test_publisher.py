3ae33aac14b3d24cece8163bef77aba66d1a0c41
Jack Leland <jack.e.leland@gmail.com>
2022-08-03 08:41:29 +0100

    Fix and extend publisher create message tests

--- tests/nlds/rabbit/test_publisher.py
+++ tests/nlds/rabbit/test_publisher.py
@@ -35,31 +35,60 @@ def message_assertions(message):
     assert RabbitMQPublisher.MSG_USER in message_details
     assert RabbitMQPublisher.MSG_GROUP in message_details
     assert RabbitMQPublisher.MSG_TARGET in message_details
+    assert RabbitMQPublisher.MSG_ACCESS_KEY in message_details
+    assert RabbitMQPublisher.MSG_SECRET_KEY in message_details
+    assert RabbitMQPublisher.MSG_TENANCY in message_details
 
     # Check message contains expected data
     assert RabbitMQPublisher.MSG_DATA in message
     message_data = message[RabbitMQPublisher.MSG_DATA]
     assert RabbitMQPublisher.MSG_FILELIST in message_data
 
+    assert RabbitMQPublisher.MSG_TYPE in message
+
 def test_constructor(default_publisher):
     # Check that the publisher with callback can be created
     assert default_publisher.whole_config['authentication']['jasmin_authenticator']['user_profile_url'] == "{{ user_profile_url }}"
 
 @pytest.mark.parametrize("uuid", [123, "123"])
-@pytest.mark.parametrize("filelist", ["test_file.txt", "[test_1, test_2]"])
+@pytest.mark.parametrize("filelist", [
+    "test_file.txt", 
+    "[test_1, test_2]", 
+    ["test_1", "test_2"],
+    [123, 100],
+])
 def test_create_message(test_uuid, uuid, filelist):
     # Test with definitely functional data
-    message = RabbitMQPublisher.create_message(test_uuid, "data", "user", "group", "target")
+    message = RabbitMQPublisher.create_message(
+        test_uuid, 
+        ["data", "data_again"], 
+        "access_key",
+        "secret_key", 
+        user="user",
+        group="group", 
+        target="target",
+        tenancy="tenancy"
+    )
     message_assertions(message)
 
     # Test whether optional args are optional
-    message = RabbitMQPublisher.create_message(test_uuid, "data")
+    message = RabbitMQPublisher.create_message(test_uuid, ["data_0", "data_1"],  
+                                               "access_key", "secret_key")
     message_assertions(message)
 
+    # Test with definitely non-functional data
+    with pytest.raises(TypeError):
+        message = RabbitMQPublisher.create_message(test_uuid, 123, 
+                                                   "access_key", "secret_key")
+        message = RabbitMQPublisher.create_message(test_uuid, {"k": "v"}, 
+                                                   "access_key", "secret_key")
+
     # Test with the parameterisations
-    message = RabbitMQPublisher.create_message(test_uuid, filelist)
+    message = RabbitMQPublisher.create_message(test_uuid, filelist, 
+                                               "access_key", "secret_key")
     message_assertions(message)
-    message = RabbitMQPublisher.create_message(uuid, filelist)
+    message = RabbitMQPublisher.create_message(uuid, filelist, 
+                                               "access_key", "secret_key")
     message_assertions(message)
 
 def test_publish_message(default_publisher):
