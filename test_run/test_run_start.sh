e5bc323a1807b0fe5c545a3e1553bbc957cdf816
Neil Massey <neil.massey@stfc.ac.uk>
2023-01-25 14:42:47 +0000

    Renamed test_run

--- test_run/test_run_start.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-if [[ ! -d ~/nlds_log ]]
-then
-    mkdir ~/nlds_log
-fi
-
-source $HOME/python-venvs/nlds-venv/bin/activate
-# start a named screen session
-screen -S nlds -c test_run.rc 
