bc611a647301acd560a61e5836c9d29bc6bf4175
Neil Massey <neil.massey@stfc.ac.uk>
2023-03-07 15:59:39 +0000

    Removing stripping whitespace from tags

--- nlds/utils/process_tag.py
+++ nlds/utils/process_tag.py
@@ -1,15 +1,16 @@
 def process_tag(tag):
     """Process a tag in string format into dictionary format"""
-    try:
+    # try:
+    if True:
         tag_dict = {}
-        # strip whitespace and "{" "}" symbolsfirst
-        tag_list = (tag.replace(" ","").replace("{", "").replace("}", "")
+        # strip "{" "}" symbolsfirst
+        tag_list = (tag.replace("{", "").replace("}", "")
                     ).split(",")
         for tag_i in tag_list:
             tag_kv = tag_i.split(":")
             if len(tag_kv) < 2:
                 continue
             tag_dict[tag_kv[0]] = tag_kv[1]
-    except: # what exceptions might be raised here?
-        raise ValueError
+    # except: # what exceptions might be raised here?
+    #     raise ValueError
     return tag_dict
