c97887e4cc07422c6bbf3e97f441eb6e7b4071a8
Jack Leland <jack.e.leland@gmail.com>
2023-03-13 15:42:42 +0000

    Add some unit tests for db-mixin, catalog and monitor

---
+++ tests/nlds_processors/catalog/test_catalog_worker.py
@@ -0,0 +1,20 @@
+import pytest
+import functools
+
+from nlds.rabbit import publisher as publ
+import nlds.rabbit.consumer as cons
+from nlds.details import PathDetails
+from nlds_processors.catalog.catalog_worker import CatalogConsumer
+
+def mock_load_config(template_config):
+    return template_config
+
+@pytest.fixture()
+def default_catalog(monkeypatch, template_config):
+    # Ensure template is loaded instead of .server_config
+    monkeypatch.setattr(publ, "load_config", functools.partial(
+        mock_load_config, 
+        template_config
+        )
+    )
+    return CatalogConsumer()
