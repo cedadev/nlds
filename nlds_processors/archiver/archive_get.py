b6478c606ca39c4d801e7671215c5644db0c63c7
Jack Leland <jack.e.leland@gmail.com>
2023-06-01 15:38:04 +0100

    Remove debug print statements

--- nlds_processors/archiver/archive_get.py
+++ nlds_processors/archiver/archive_get.py
@@ -44,7 +44,6 @@ class GetArchiveConsumer(BaseArchiveConsumer):
 
 
     def __init__(self, queue=DEFAULT_QUEUE_NAME):
-        print("Test print")
         super().__init__(queue=queue)
 
 
@@ -178,8 +177,6 @@ class GetArchiveConsumer(BaseArchiveConsumer):
                     continue
                 
                 with client.File() as f:
-                    print("###Print inside client.File() block###")
-
                     # Open the file with READ
                     status, _ = f.open(tape_full_path, OpenFlags.READ)
                     if status.status != 0:
@@ -187,8 +184,6 @@ class GetArchiveConsumer(BaseArchiveConsumer):
 
                     # Wrap the File handler so minio can use it 
                     fw = XRDFileWrapper(f)
-
-                    print("###Print after FileWrapper declaration###")
 
                     # Ensure minimum part_size is met
                     chunk_size = max(5*1024*1024, self.chunk_size)
