27251afb6b39cf117d4fde584ce4b9962c1dfd70
william cross <william.cross@stfc.ac.uk>
2023-09-25 09:59:46 +0100

    didnt work

--- nlds/routers/__init__.py
+++ nlds/routers/__init__.py
@@ -5,16 +5,16 @@
 __author__ = 'Neil Massey and Jack Leland'
 __date__ = '30 Nov 2021'
 __copyright__ = 'Copyright 2021 United Kingdom Research and Innovation'
-# __license__ = 'BSD - see LICENSE file in top-level package directory'
-# __contact__ = 'neil.massey@stfc.ac.uk'
+__license__ = 'BSD - see LICENSE file in top-level package directory'
+__contact__ = 'neil.massey@stfc.ac.uk'
 
-# from ..rabbit.publisher import RabbitMQPublisher
-# from ..rabbit.rpc_publisher import RabbitMQRPCPublisher
+from ..rabbit.publisher import RabbitMQPublisher
+from ..rabbit.rpc_publisher import RabbitMQRPCPublisher
 
-# # Create a publisher and start its connection
-# rabbit_publisher = RabbitMQPublisher()
-# rabbit_publisher.get_connection()
+# Create a publisher and start its connection
+rabbit_publisher = RabbitMQPublisher()
+rabbit_publisher.get_connection()
 
-# # Create a RPC publisher and start its connection
-# rpc_publisher = RabbitMQRPCPublisher()
-# rpc_publisher.get_connection()
+# Create a RPC publisher and start its connection
+rpc_publisher = RabbitMQRPCPublisher()
+rpc_publisher.get_connection()
