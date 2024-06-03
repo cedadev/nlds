5859b13c2c0c5a1cda613015ffd238696f40c451
Nicola Farmer <nicola.farmer@stfc.ac.uk>
2024-06-03 15:36:37 +0100

    Black.

--- nlds/authenticators/jasmin_authenticator.py
+++ nlds/authenticators/jasmin_authenticator.py
@@ -2,17 +2,18 @@
 """
 
 """
-__author__ = 'Neil Massey and Jack Leland'
-__date__ = '30 Nov 2021'
-__copyright__ = 'Copyright 2021 United Kingdom Research and Innovation'
-__license__ = 'BSD - see LICENSE file in top-level package directory'
-__contact__ = 'neil.massey@stfc.ac.uk'
+__author__ = "Neil Massey and Jack Leland"
+__date__ = "30 Nov 2021"
+__copyright__ = "Copyright 2021 United Kingdom Research and Innovation"
+__license__ = "BSD - see LICENSE file in top-level package directory"
+__contact__ = "neil.massey@stfc.ac.uk"
 
 from .base_authenticator import BaseAuthenticator
 from ..server_config import load_config
 from retry import retry
 import requests
 import json
+
 
 class JasminAuthenticator(BaseAuthenticator):
 
@@ -23,19 +24,18 @@ class JasminAuthenticator(BaseAuthenticator):
         self.name = "jasmin_authenticator"
         self.auth_name = "authentication"
 
-
     @retry(requests.ConnectTimeout, tries=5, delay=1, backoff=2)
     def authenticate_token(self, oauth_token: str):
         """Make a call to the JASMIN token introspection to determine whether
         the token is a true token and whether it is valid."""
         config = self.config[self.auth_name][self.name]
         token_headers = {
-            "Content-Type" : "application/x-www-form-urlencoded",
+            "Content-Type": "application/x-www-form-urlencoded",
             "cache-control": "no-cache",
-            "Authorization" : f"Bearer {oauth_token}"
+            "Authorization": f"Bearer {oauth_token}",
         }
         token_data = {
-            "token" : oauth_token,
+            "token": oauth_token,
         }
         # contact the oauth_token_introspect_url to check the token
         # we expect a 200 status code to be returned
@@ -44,10 +44,10 @@ class JasminAuthenticator(BaseAuthenticator):
         ### and user errors
         try:
             response = requests.post(
-                config['oauth_token_introspect_url'],
-                data = token_data,
-                headers = token_headers,
-                timeout = JasminAuthenticator._timeout,
+                config["oauth_token_introspect_url"],
+                data=token_data,
+                headers=token_headers,
+                timeout=JasminAuthenticator._timeout,
             )
         except requests.exceptions.ConnectionError:
             raise RuntimeError(
@@ -64,7 +64,7 @@ class JasminAuthenticator(BaseAuthenticator):
         if response.status_code == requests.codes.ok:  # status code 200
             try:
                 response_json = json.loads(response.text)
-                return response_json['active']
+                return response_json["active"]
             except KeyError:
                 raise RuntimeError(
                     "The 'active' key was not found in the response from "
@@ -79,16 +79,15 @@ class JasminAuthenticator(BaseAuthenticator):
 
         return False
 
-
     @retry(requests.ConnectTimeout, tries=5, delay=1, backoff=2)
     def authenticate_user(self, oauth_token: str, user: str):
         """Make a call to the JASMIN services for a user to determine whether
         the user with the token is a valid user."""
         config = self.config[self.auth_name][self.name]
         token_headers = {
-            "Content-Type" : "application/x-www-form-urlencoded",
+            "Content-Type": "application/x-www-form-urlencoded",
             "cache-control": "no-cache",
-            "Authorization" : f"Bearer {oauth_token}"
+            "Authorization": f"Bearer {oauth_token}",
         }
         # contact the user_profile_url to check the token and check that the
         # user in the profile matches the user in the parameter
@@ -97,9 +96,9 @@ class JasminAuthenticator(BaseAuthenticator):
         # we expect a 200 status code to be returned
         try:
             response = requests.get(
-                config['user_profile_url'],
-                headers = token_headers,
-                timeout = JasminAuthenticator._timeout,
+                config["user_profile_url"],
+                headers=token_headers,
+                timeout=JasminAuthenticator._timeout,
             )
         except requests.exceptions.ConnectionError:
             raise RuntimeError(
@@ -115,7 +114,7 @@ class JasminAuthenticator(BaseAuthenticator):
         if response.status_code == requests.codes.ok:  # status code 200
             try:
                 response_json = json.loads(response.text)
-                return response_json['username'] == user
+                return response_json["username"] == user
             except KeyError:
                 raise RuntimeError(
                     "The 'username' key was not found in the response from "
@@ -131,16 +130,15 @@ class JasminAuthenticator(BaseAuthenticator):
             return False
         return user
 
-
     @retry(requests.ConnectTimeout, tries=5, delay=1, backoff=2)
     def authenticate_group(self, oauth_token: str, group: str):
         """Make a call to the JASMIN services for a user to determine whether
         the user with the token is part of the requested group."""
         config = self.config[self.auth_name][self.name]
         token_headers = {
-            "Content-Type" : "application/x-www-form-urlencoded",
+            "Content-Type": "application/x-www-form-urlencoded",
             "cache-control": "no-cache",
-            "Authorization" : f"Bearer {oauth_token}"
+            "Authorization": f"Bearer {oauth_token}",
         }
         # contact the user_services_url to check the token and check that one
         # of the groups listed in the services matches the group in the
@@ -149,9 +147,9 @@ class JasminAuthenticator(BaseAuthenticator):
         # manner.  Hurrah!
         try:
             response = requests.get(
-                config['user_services_url'],
-                headers = token_headers,
-                timeout = JasminAuthenticator._timeout
+                config["user_services_url"],
+                headers=token_headers,
+                timeout=JasminAuthenticator._timeout,
             )
         except requests.exceptions.ConnectionError:
             raise RuntimeError(
@@ -167,7 +165,7 @@ class JasminAuthenticator(BaseAuthenticator):
         if response.status_code == requests.codes.ok:  # status code 200
             try:
                 response_json = json.loads(response.text)
-                user_gws = response_json['group_workspaces']
+                user_gws = response_json["group_workspaces"]
                 return group in user_gws
             except KeyError:
                 raise RuntimeError(
@@ -183,26 +181,25 @@ class JasminAuthenticator(BaseAuthenticator):
         else:
             return False
 
-
     @retry(requests.ConnectTimeout, tries=5, delay=1, backoff=2)
     def authenticate_user_group_role(self, oauth_token: str, user: str, group: str):
         """Make a call to the JASMIN services to determine whether the user with the
         token has a manager/deputy role within the requested group."""
         config = self.config[self.auth_name][self.name]
         token_headers = {
-            "Content-Type" : "application/x-www-form-urlencoded",
+            "Content-Type": "application/x-www-form-urlencoded",
             "cache-control": "no-cache",
-            "Authorization" : f"Bearer {oauth_token}"
+            "Authorization": f"Bearer {oauth_token}",
         }
         # Contact the user_grants_url to check the role of the user in the group
-        # given. This checks whether the user is a manger, deputy or user and 
-        # returns True if they are either a manager or deputy, otherwise it 
+        # given. This checks whether the user is a manger, deputy or user and
+        # returns True if they are either a manager or deputy, otherwise it
         # returns False.
         try:
             response = requests.get(
                 f"{config['user_grants_url']}{user}/grants/?category=GWS&service={group}",
-                headers = token_headers,
-                timeout = JasminAuthenticator._timeout
+                headers=token_headers,
+                timeout=JasminAuthenticator._timeout,
             )
         except requests.exceptions.ConnectionError:
             raise RuntimeError(
@@ -218,8 +215,8 @@ class JasminAuthenticator(BaseAuthenticator):
         if response.status_code == requests.codes.ok:  # status code 200
             try:
                 response_json = json.loads(response.text)
-                user_role = response_json['group_workspaces']
-                return user_role in ['MANAGER', 'DEPUTY']
+                user_role = response_json["group_workspaces"]
+                return user_role in ["MANAGER", "DEPUTY"]
             except KeyError:
                 raise RuntimeError(
                     "The user's role was not found in the response "
