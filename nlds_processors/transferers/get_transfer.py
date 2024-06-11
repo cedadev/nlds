bfc26d4ebdd944bc5df072d11cfe6789af02e71c
Neil Massey <neil.massey@stfc.ac.uk>
2024-06-11 17:15:33 +0100

    Refactored get_transfer

--- nlds_processors/transferers/get_transfer.py
+++ nlds_processors/transferers/get_transfer.py
@@ -32,6 +32,120 @@ class GetTransferConsumer(BaseTransferConsumer):
 
         self.chown_cmd = self.load_config_value(self._CHOWN_COMMAND)
         self.chown_fl = self.load_config_value(self._CHOWN_FL)
+        self.client = None
+
+    def _get_target_path(self, body_json: Dict):
+        # Get target and verify it can be written to
+        target = body_json[MSG.DETAILS][MSG.TARGET]
+        if target:
+            target_path = Path(target)
+            if not self.check_path_access(target_path, access=os.W_OK):
+                self.log(f"Full path: {target_path}", RK.LOG_DEBUG)
+                raise CallbackError(
+                    "Unable to copy, given target path is "
+                    "inaccessible. Passing for retry."
+                )
+        else:
+            target_path = None
+        return target_path
+
+    def _get_and_check_bucket_name_object_name(self, path_details):
+        """Get the bucket and object name and perform an existence check on the
+        bucket"""
+        if len(path_details.object_name.split(":")) == 2:
+            bucket_name, object_name = path_details.object_name.split(":")
+        # Otherwise, log error and queue for retry
+        else:
+            reason = "Unable to get bucket_name from message info"
+            self.log(
+                f"{reason}, adding " f"{path_details.object_name} to failed list.",
+                RK.LOG_INFO,
+            )
+            return None, None, reason
+
+        if bucket_name and not self.client.bucket_exists(bucket_name):
+            # If bucket doesn't exist then pass for failure
+            reason = f"Bucket {bucket_name} does not exist"
+            self.log(f"{reason}. Adding {object_name} to failed list.", RK.LOG_ERROR)
+            return None, None, reason
+
+        return bucket_name, object_name, None
+
+    def _get_download_path(self, path_details, target_path):
+        # Decide whether to prepend target path or download directly to it.
+        if not target_path:
+            # In the case of no given target, we just download the files
+            # back to their original location.
+            download_path = path_details.path
+            # Check we have permission to write to the parent folder of the
+            # original location. If the parent folder doesn't exist this
+            # will fail.
+            if not self.check_path_access(path_details.path.parent, access=os.W_OK):
+                reason = (
+                    f"Unable to download {download_path}.  Target "
+                    "path is inaccessible."
+                )
+                self.log(f"{reason}. Adding to failed list.", RK.LOG_INFO)
+                return None, reason
+
+        elif target_path.is_dir():
+            # In the case of a given target, we remove the leading slash on
+            # the original path and prepend the target_path
+            if path_details.original_path[0] == "/":
+                download_path = target_path / path_details.original_path[1:]
+            else:
+                download_path = target_path / path_details.original_path
+        else:
+            # TODO (2022-09-20): This probably isn't appropriate for getlist
+            self.log(
+                "Target path is not a valid directory, renaming files "
+                "got to target path.",
+                RK.LOG_WARNING,
+            )
+            download_path = target_path
+        return download_path, None
+
+    def _transfer(self, bucket_name, object_name, download_path):
+        download_path_str = str(download_path)
+        # Attempt the download!
+        try:
+            resp = self.client.fget_object(
+                bucket_name,
+                object_name,
+                download_path_str,
+            )
+            return None
+        except Exception as e:
+            reason = f"Download-time exception occurred: {e}"
+            self.log(reason, RK.LOG_DEBUG)
+            self.log(
+                f"Exception encountered during download, adding "
+                f"{object_name} to failed list.",
+                RK.LOG_INFO,
+            )
+            return reason
+
+    def _change_permissions(self, download_path, path_details):
+        """Change the permission of the downloaded files"""
+        download_path_str = str(download_path)
+        self.log(
+            f"Changing permissions and ownership of file " f"{download_path}",
+            RK.LOG_ERROR,
+        )
+        try:
+            # Attempt to change back to original permissions
+            os.chmod(download_path, mode=path_details.permissions)
+        except (KeyError, PermissionError) as e:
+            self.log("Couldn't change permissions of downloaded file", RK.LOG_WARNING)
+            self.log(f"Original error: {e}", RK.LOG_DEBUG)
+
+        # Attempt to chown the path back to the requesting user using the
+        # configured binary/command.
+        try:
+            subprocess.run([self.chown_cmd, str(self.uid), download_path_str])
+        except (KeyError, PermissionError) as e:
+            self.log("Couldn't change owner of downloaded file", RK.LOG_WARNING)
+            self.log(f"Original error: {e}", RK.LOG_DEBUG)
 
     @retry(S3Error, tries=5, delay=1, logger=None)
     def transfer(
@@ -44,27 +158,14 @@ class GetTransferConsumer(BaseTransferConsumer):
         rk_origin: str,
         body_json: Dict[str, Any],
     ) -> None:
-        # Grab message details for ease of access. This shouldn't fail becuase
-        # of checking done in the worker, might make sense to do it here/in the
-        # basetransfer too though
-        msg_details = body_json[MSG.DETAILS]
-
-        # Get target and verify it can be written to
-        target = msg_details[MSG.TARGET]
-        if target:
-            target_path = Path(target)
-            if not self.check_path_access(target_path, access=os.W_OK):
-                self.log(f"Full path: {target_path}", RK.LOG_DEBUG)
-                raise CallbackError(
-                    "Unable to copy, given target path is "
-                    "inaccessible. Passing for retry."
-                )
-
+        # get the target
+        target_path = self._get_target_path(body_json)
+        # set the ids for the
         if self.chown_fl:
             self.set_ids(body_json)
 
-        # Create client!
-        client = minio.Minio(
+        # Create client
+        self.client = minio.Minio(
             tenancy,
             access_key=access_key,
             secret_key=secret_key,
@@ -76,33 +177,12 @@ class GetTransferConsumer(BaseTransferConsumer):
 
         for path_details in filelist:
             # If bucketname inserted into object path (i.e. from catalogue) then
-            # extract both - TODO, get these from the path_details via a function
-            if len(path_details.object_name.split(":")) == 2:
-                bucket_name, object_name = path_details.object_name.split(":")
-            # Otherwise, log error and queue for retry
-            else:
-                reason = "Unable to get bucket_name from message info"
-                self.log(
-                    f"{reason}, adding " f"{path_details.object_name} to failed list.",
-                    RK.LOG_INFO,
-                )
-                path_details.failure_reason = reason
-                self.append_and_send(
-                    self.failedlist,
-                    path_details,
-                    routing_key=rk_failed,
-                    body_json=body_json,
-                    state=State.FAILED,
-                )
-                continue
-
-            if bucket_name and not client.bucket_exists(bucket_name):
-                # If bucket doesn't exist then pass for failure
-                reason = f"Bucket {bucket_name} does not exist"
-                self.log(
-                    f"{reason}. Adding {object_name} to failed list.", RK.LOG_ERROR
-                )
-                path_details.failure_reason = reason
+            # extract both
+            bucket_name, object_name, failure_reason = (
+                self._get_and_check_bucket_name_object_name(path_details)
+            )
+            if failure_reason:
+                path_details.failure_reason = failure_reason
                 self.append_and_send(
                     self.failedlist,
                     path_details,
@@ -115,64 +195,13 @@ class GetTransferConsumer(BaseTransferConsumer):
             self.log(
                 f"Attempting to get file {object_name} from {bucket_name}", RK.LOG_DEBUG
             )
-            # Decide whether to prepend target path or download directly to it.
-            if not target:
-                # In the case of no given target, we just download the files
-                # back to their original location.
-                download_path = path_details.path
-                # Check we have permission to write to the parent folder of the
-                # original location. If the parent folder doesn't exist this
-                # will fail.
-                if not self.check_path_access(path_details.path.parent, access=os.W_OK):
-                    reason = (
-                        f"Unable to download {download_path}.  Target "
-                        "path is inaccessible."
-                    )
-                    self.log(f"{reason}. Adding to failed list.", RK.LOG_INFO)
-                    path_details.failure_reason = reason
-                    self.append_and_send(
-                        self.failedlist,
-                        path_details,
-                        routing_key=rk_failed,
-                        body_json=body_json,
-                        state=State.FAILED,
-                    )
-                    continue
-            elif target_path.is_dir():
-                # In the case of a given target, we remove the leading slash on
-                # the original path and prepend the target_path
-                if path_details.original_path[0] == "/":
-                    download_path = target_path / path_details.original_path[1:]
-                else:
-                    download_path = target_path / path_details.original_path
-            else:
-                # TODO (2022-09-20): This probably isn't appropriate for getlist
-                self.log(
-                    "Target path is not a valid directory, renaming files "
-                    "gotten to target path.",
-                    RK.LOG_WARNING,
-                )
-                download_path = target_path
 
-            # Cast to string
-            download_path_str = str(download_path)
-
-            # Attempt the download!
-            try:
-                resp = client.fget_object(
-                    bucket_name,
-                    object_name,
-                    download_path_str,
-                )
-            except Exception as e:
-                reason = f"Download-time exception occurred: {e}"
-                self.log(reason, RK.LOG_DEBUG)
-                self.log(
-                    f"Exception encountered during download, adding "
-                    f"{object_name} to failed list.",
-                    RK.LOG_INFO,
-                )
-                path_details.failure_reason = reason
+            # get the download path from the path details
+            download_path, failure_reason = self._get_download_path(
+                path_details, target_path
+            )
+            if failure_reason:
+                path_details.failure_reason = failure_reason
                 self.append_and_send(
                     self.failedlist,
                     path_details,
@@ -182,27 +211,23 @@ class GetTransferConsumer(BaseTransferConsumer):
                 )
                 continue
 
-            self.log(
-                f"Changing permissions and ownership of file " f"{download_path}",
-                RK.LOG_ERROR,
-            )
-            try:
-                # Attempt to change back to original permissions
-                os.chmod(download_path, mode=path_details.permissions)
-            except (KeyError, PermissionError) as e:
-                self.log(
-                    "Couldn't change permissions of downloaded file", RK.LOG_WARNING
+            # do the download
+            failure_reason = self._transfer(bucket_name, object_name, download_path)
+            if failure_reason:
+                path_details.failure_reason = failure_reason
+                self.append_and_send(
+                    self.failedlist,
+                    path_details,
+                    routing_key=rk_failed,
+                    body_json=body_json,
+                    state=State.FAILED,
                 )
-                self.log(f"Original error: {e}", RK.LOG_DEBUG)
+                continue
 
-            # Attempt to chown the path back to the requesting user using the
-            # configured binary/command.
-            try:
-                subprocess.run([self.chown_cmd, str(self.uid), download_path_str])
-            except (KeyError, PermissionError) as e:
-                self.log("Couldn't change owner of downloaded file", RK.LOG_WARNING)
-                self.log(f"Original error: {e}", RK.LOG_DEBUG)
+            # change ownership and permissions
+            self._change_permissions(download_path)
 
+            # all finished successfully!
             self.log(f"Successfully got {path_details.original_path}", RK.LOG_DEBUG)
             self.append_and_send(
                 self.completelist,
