8f96f1300c0dc7814f8b824c5b787df6831642fa
Nicola Farmer <nicola.farmer@stfc.ac.uk>
2024-08-05 16:49:36 +0100

    Black formatting.

--- nlds/utils/get_quotas.py
+++ nlds/utils/get_quotas.py
@@ -5,9 +5,7 @@ import requests
 import json
 
 
-
-
-class Quotas():
+class Quotas:
 
     _timeout = 10.0
 
@@ -26,7 +24,7 @@ class Quotas():
             "Authorization": f"Bearer {oauth_token}",
         }
         # Contact the user_services_url to get the information about the services
-        url = construct_url([config["user_services_url"]], {"name":{service_name}})
+        url = construct_url([config["user_services_url"]], {"name": {service_name}})
         try:
             response = requests.get(
                 url,
@@ -36,25 +34,28 @@ class Quotas():
         except requests.exceptions.ConnectionError:
             raise RuntimeError(f"User services url {url} could not be reached.")
         except KeyError:
-            raise RuntimeError(f"Could not find 'user_services_url' key in the {self.name} section of the .server_config file.")
-        if response.status_code == requests.codes.ok: # status code 200
+            raise RuntimeError(
+                f"Could not find 'user_services_url' key in the {self.name} section of the .server_config file."
+            )
+        if response.status_code == requests.codes.ok:  # status code 200
             try:
                 response_json = json.loads(response.text)
                 return response_json
             except json.JSONDecodeError:
-                raise RuntimeError(f"Invalid JSON returned from the user services url: {url}")
+                raise RuntimeError(
+                    f"Invalid JSON returned from the user services url: {url}"
+                )
         else:
             raise RuntimeError(f"Error getting data for {service_name}")
-        
-    
+
     def extract_tape_quota(self, oauth_token: str, service_name):
         """Get the service information then process it to extract the quota for the service."""
         # Try to get the service information and throw an exception if an error is encountered
         try:
             result = self.get_projects_services(self, oauth_token, service_name)
-        except(RuntimeError, ValueError) as e:
+        except (RuntimeError, ValueError) as e:
             raise type(e)(f"Error getting information for {service_name}: {e}")
-        
+
         # Process the result to get the requirements
         for attr in result:
             # Check that the category is Group Workspace
@@ -63,10 +64,14 @@ class Quotas():
                 if attr["requirements"]:
                     requirements = attr["requirements"]
                 else:
-                    raise ValueError(f"Cannot find any requirements for {service_name}.")
+                    raise ValueError(
+                        f"Cannot find any requirements for {service_name}."
+                    )
             else:
-                raise ValueError(f"Cannot find a Group Workspace with the name {service_name}. Check the category.")
-                
+                raise ValueError(
+                    f"Cannot find a Group Workspace with the name {service_name}. Check the category."
+                )
+
         # Go through the requirements to find the tape resource requirement
         for requirement in requirements:
             # Only return provisioned requirements
@@ -78,10 +83,18 @@ class Quotas():
                         if tape_quota:
                             return tape_quota
                         else:
-                            raise ValueError(f"Issue getting tape quota for {service_name}. Quota is zero.")
+                            raise ValueError(
+                                f"Issue getting tape quota for {service_name}. Quota is zero."
+                            )
                     except KeyError:
-                        raise KeyError(f"Issue getting tape quota for {service_name}. No 'value' field exists.")
+                        raise KeyError(
+                            f"Issue getting tape quota for {service_name}. No 'value' field exists."
+                        )
                 else:
-                    raise ValueError(f"No tape resources could be found for {service_name}")
+                    raise ValueError(
+                        f"No tape resources could be found for {service_name}"
+                    )
             else:
-                raise ValueError(f"No provisioned requirements found for {service_name}. Check the status of your requested resources.")
+                raise ValueError(
+                    f"No provisioned requirements found for {service_name}. Check the status of your requested resources."
+                )
