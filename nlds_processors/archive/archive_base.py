e2af306590db172fb1130a505117b07f19b95ace
Neil Massey <neil.massey@stfc.ac.uk>
2024-11-19 15:29:00 +0000

    Removed an outdated comment

--- nlds_processors/archive/archive_base.py
+++ nlds_processors/archive/archive_base.py
@@ -1,8 +1,6 @@
 # encoding: utf-8
 """
 archive_base.py
-NOTE: This module is imported into a revision, and so should be very defensive 
-with how it imports external modules (like xrootd). 
 """
 __author__ = "Jack Leland and Neil Massey"
 __date__ = "30 Nov 2021"
