a1f41b2b6139cf45399cfea066908f15bf8359af
Nicola Farmer <nicola.farmer@stfc.ac.uk>
2025-06-25 12:03:17 +0100

    Create migration file

---
+++ migrations/versions/2025-06-25-1200_66539918fe94_create_quota_row_in_catalog.py
@@ -0,0 +1,44 @@
+"""create quota row in catalog
+
+Revision ID: 66539918fe94
+Revises: 87bbd9e274a1
+Create Date: 2025-06-25 12:00:48.246801
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '66539918fe94'
+down_revision = '87bbd9e274a1'
+branch_labels = None
+depends_on = None
+
+
+def upgrade(engine_name: str) -> None:
+    globals()["upgrade_%s" % engine_name]()
+
+
+def downgrade(engine_name: str) -> None:
+    globals()["downgrade_%s" % engine_name]()
+
+
+
+
+
+def upgrade_catalog() -> None:
+    pass
+
+
+def downgrade_catalog() -> None:
+    pass
+
+
+def upgrade_monitor() -> None:
+    pass
+
+
+def downgrade_monitor() -> None:
+    pass
+
