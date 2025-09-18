5f37ffb1371811f3a2bce7cbd5974f23c84b1d83
Neil Massey <neil.massey@stfc.ac.uk>
2025-09-18 15:51:11 +0100

    Pass uuid as a string rather than a function

--- nlds_processors/archive/send_archive_next.py
+++ nlds_processors/archive/send_archive_next.py
@@ -1,8 +1,8 @@
 # encoding: utf-8
 """
 send_archive_next.py
-NOTE: This module is imported into a revision, and so should be very defensive 
-with how it imports external modules (like xrootd). 
+NOTE: This module is imported into a revision, and so should be very defensive
+with how it imports external modules (like xrootd).
 """
 __author__ = "Jack Leland and Neil Massey"
 __date__ = "30 Nov 2021"
@@ -36,14 +36,14 @@ def send_archive_next():
     if CRONJOB_CONFIG_SECTION in rabbit_publisher.whole_config:
         cronjob_config |= rabbit_publisher.whole_config[CRONJOB_CONFIG_SECTION]
 
-    uuid = str(uuid4)
+    uuid = str(uuid4())
     msg_dict = {
         MSG.DETAILS: {
             MSG.TRANSACT_ID: uuid,
             # for the root message, the sub_id is the transaction_id
             MSG.SUB_ID: uuid,
             MSG.TARGET: None,
-            MSG.API_ACTION: "archive-put",
+            MSG.API_ACTION: RK.ARCHIVE_PUT,
             MSG.JOB_LABEL: "archive-next",
             MSG.USER: "admin-placeholder",
             MSG.GROUP: "admin-placeholder",
@@ -64,7 +64,6 @@ def send_archive_next():
     click.echo(f"Sending message to {routing_key}: \n{json.dumps(msg_dict, indent=4)}")
     rabbit_publisher.publish_message(routing_key, msg_dict)
     click.echo("Message sent!")
-
 
 if __name__ == "__main__":
     send_archive_next()
