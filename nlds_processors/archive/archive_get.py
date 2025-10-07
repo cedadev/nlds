b96dce835fef97d0fe965c54f0f7bb4551fcd710
Neil Massey <neil.massey@stfc.ac.uk>
2025-10-07 09:23:10 +0100

    Changed delay time from 60000ms to 60s

--- nlds_processors/archive/archive_get.py
+++ nlds_processors/archive/archive_get.py
@@ -1,8 +1,8 @@
 # encoding: utf-8
 """
 archive_get.py
-NOTE: This module is imported into a revision, and so should be very defensive 
-with how it imports external modules (like xrootd). 
+NOTE: This module is imported into a revision, and so should be very defensive
+with how it imports external modules (like xrootd).
 """
 __author__ = "Jack Leland and Neil Massey"
 __date__ = "30 Nov 2021"
@@ -30,7 +30,7 @@ class GetArchiveConsumer(BaseArchiveConsumer):
     DEFAULT_QUEUE_NAME = "archive_get_q"
     DEFAULT_ROUTING_KEY = f"{RK.ROOT}." f"{RK.ARCHIVE_GET}." f"{RK.WILD}"
     DEFAULT_STATE = State.ARCHIVE_GETTING
-    PREPARE_DELAY = 60 * 1000 # 60 seconds delay between PREPARE_check requests
+    PREPARE_DELAY = 60  # 60 seconds delay between PREPARE_check requests
 
     def __init__(self, queue=DEFAULT_QUEUE_NAME):
         self.preparelist = []
@@ -260,7 +260,7 @@ class GetArchiveConsumer(BaseArchiveConsumer):
                     routing_key=rk_check,
                     body_json=body_json_check,
                     state=State.ARCHIVE_PREPARING,
-                    delay=GetArchiveConsumer.PREPARE_DELAY
+                    delay=GetArchiveConsumer.PREPARE_DELAY,
                 )
 
         if len(self.failedlist) > 0:
@@ -336,8 +336,10 @@ class GetArchiveConsumer(BaseArchiveConsumer):
                         path_details.failure_reason = e.message
                         self.failedlist.append(path_details)
 
-        # only three outcomes here - 1. either all the tarfiles (and, by extension, all
-        # files) are complete, 2. are not complete, or 3. everything failed
+        # only three outcomes here -
+        # 1. either all the tarfiles (and, by extension, all files) are complete
+        # 2. are not complete, or
+        # 3. everything failed
         if len(self.failedlist) > 0:
             self.send_pathlist(
                 self.failedlist,
@@ -374,7 +376,7 @@ class GetArchiveConsumer(BaseArchiveConsumer):
                     routing_key=rk_check,
                     body_json=body_json,
                     state=State.ARCHIVE_PREPARING,
-                    delay=GetArchiveConsumer.PREPARE_DELAY
+                    delay=GetArchiveConsumer.PREPARE_DELAY,
                 )
 
 
