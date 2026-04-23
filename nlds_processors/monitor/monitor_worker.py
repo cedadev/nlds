367a7c276f3f5ed43e9ef9ccb29ee3bdf8caf1a3
nmassey001 <neil.massey@stfc.ac.uk>
2026-04-23 10:57:44 +0100

    Allow queue name to be passed in so RPC handler can use a separate queue

--- nlds_processors/monitor/monitor_worker.py
+++ nlds_processors/monitor/monitor_worker.py
@@ -26,6 +26,7 @@ Requires these settings in the /etc/nlds/server_config file:
 """
 
 from typing import Dict
+import sys
 
 from retry.api import retry_call
 
@@ -750,7 +751,11 @@ class MonitorConsumer(RMQC):
 
 
 def main():
-    consumer = MonitorConsumer()
+    if len(sys.argv) > 1:
+        queue_name = sys.argv[1]
+    else:
+        queue_name = MonitorConsumer.DEFAULT_QUEUE_NAME
+    consumer = MonitorConsumer(queue=queue_name)
     # connect to message queue early so that we can send logging messages about
     # connecting to the database
     consumer.get_connection()
